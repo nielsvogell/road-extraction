149ccf84bcdb472760de33907be2c041d6f82b9b
Niels <84444076+nielsvogell@users.noreply.github.com>
2022-02-23 13:12:52 +0100

    Added extraction script until the first thinning stage

--- extraction.py
+++ extraction.py
@@ -1,14 +1,81 @@
 import numpy as np
 import cv2
 import segmentation
+import pickle
 from matplotlib import use
 use('TkAgg')
 import matplotlib.pyplot as plt
 
+
 img_rgb = cv2.cvtColor(cv2.imread('data/exp.png'), cv2.COLOR_BGR2RGB)
 label_img, final_label = segmentation.segment(img_rgb)
 
-road_mask = np.zeros_like(label_img)
+# segmentation_obj = (label_img, final_label)
+# label_file = open('segmented.pck', 'ab')
+# pickle.dump(segmentation_obj, label_file)
+# label_file.close()
+# label_file = open('segmented.pck', 'rb')
+# segmentation_obj = pickle.load(label_file)
+# (label_img, final_label) = segmentation_obj
+# label_file.close()
 
-plt.imshow(label_img)
+
+def keep_second_largest_component(img_lbl):
+    no_components, components = cv2.connectedComponents(img_lbl)
+    component_size = np.array([np.sum(components == lbl) for lbl in range(no_components)])
+    second_largest_component = np.argpartition(component_size.flatten(), -2)[-2]
+    mask = np.zeros_like(components, dtype=np.uint8)
+    mask[components == second_largest_component] = 1
+    return mask
+
+road_mask = np.zeros_like(label_img).astype(np.uint8)
+road_mask[np.where(label_img == final_label['road'])] = 255
+
+plt.imshow(road_mask, cmap='gray')
+
+# Find connected components
+no_components, components = cv2.connectedComponents(road_mask)
+plt.clf()
+plt.imshow(components)
+
+# remove those with small surface area
+component_size = np.array([np.sum(components == lbl) for lbl in range(no_components)])
+road_component = np.argpartition(component_size.flatten(), -2)[-2]
+road_mask_main = np.zeros_like(components, dtype=np.uint8)
+road_mask_main[components == road_component] = 1
+
+plt.clf()
+plt.imshow(components)
+
+# use morph close to close holes and remove noise
+roads_closed = cv2.morphologyEx(road_mask_main, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9)))
+
+plt.clf()
+plt.imshow(roads_closed)
+
+# use open to cut away pathways with a structuring element that is smaller than the main road (main road width ~20px)
+roads_opened = cv2.morphologyEx(road_mask_main, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (13, 13)))
+plt.clf()
+plt.imshow(roads_opened)
+roads_opened = keep_second_largest_component(roads_opened)
+plt.clf()
+plt.imshow(roads_opened)
+roads_opened = cv2.morphologyEx(roads_opened, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (19, 19)))
+plt.clf()
+plt.imshow(roads_opened)
+
+# Thinning process
+roads_inv = np.ones_like(roads_opened, dtype=np.uint8)
+roads_inv[roads_opened == 1] = 0
+roads_inv = cv2.dilate(roads_inv, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)))
+border = np.logical_and(roads_inv, roads_opened)
+plt.clf()
+plt.imshow(border)
+
+
+# Find intersection points
+
+# Scan for points with at least 3 neighbors
+kernel = np.ones((3, 3), dtype=np.uint8)
+neighbors = cv2.filter2D(roads_opened, 0, kernel=kernel)
 
