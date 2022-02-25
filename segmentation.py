8f08c5ef7f9a43f29589fda838c02191174be4dd
Niels <84444076+nielsvogell@users.noreply.github.com>
2022-02-25 23:59:52 +0100

    renamed segmentation_abgabe to segmentation

--- segmentation.py
+++ segmentation.py
@@ -1,281 +1,257 @@
-# Imports
 import cv2
 import numpy as np
 from sklearn.mixture import GaussianMixture
-import pickle as pcl
-import time  # only used for profiling
 import sys
 if 'matplotlib' not in sys.modules:
     from matplotlib import use
     use('TkAgg')
 from matplotlib import pyplot as plt
-plt.switch_backend('TkAgg')
 
 
 def main():
-    # 0.25 ->  2s
-    # 0.5  -> 12s
-    # 1    -> 56s
-    img = cv2.imread("data/munich.png")
+    show_color_evaluation("data/paper_exp.png")
+    # test_segment("data/munich.png")
+
+
+def test_segment(img_path):
+    # read image
+    img = cv2.imread(img_path)
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    show_color_evaluation(img_rgb, 0.3, 7)
+
+    # SEGMENT
+    map_labels = segment(img_rgb)
+
+    # plot output
+    plt.figure(figsize=(10, 20))
+
+    # 'background': 0, 'road': 1, 'building': 2
+
+    building_mask = np.zeros_like(map_labels)
+    building_mask[np.where(map_labels == 2)] = 1
+    plt.subplot(2, 2, 1)
+    plt.axis('off')
+    plt.imshow(building_mask, cmap="gray")
+    plt.title("Buildings", fontsize=18)
+
+    road_mask = np.zeros_like(map_labels)
+    road_mask[np.where(map_labels == 1)] = 1
+    plt.subplot(2, 2, 2)
+    plt.axis('off')
+    plt.imshow(road_mask, cmap="gray")
+    plt.title("Roads", fontsize=18)
+
+    background_mask = np.zeros_like(map_labels)
+    background_mask[np.where(map_labels == 0)] = 1
+    plt.subplot(2, 2, 3)
+    plt.axis('off')
+    plt.imshow(background_mask, cmap="gray")
+    plt.title("Background", fontsize=18)
+
+    plt.tight_layout()
     plt.show()
 
 
-def show_color_evaluation(img_rgb, scale=0.3, blur_size=7):
+# calls cluster function and evaluate color function for each clustered color
+# plots the result
+def show_color_evaluation(img_path, scale=0.3, blur_size=7, nr_clusters=5):
+    img = cv2.imread(img_path)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # Cluster
-    labels, cluster_colors = gaussian_mixture_cluster(img_rgb, blur_size=blur_size, scale=scale, nr_clusters=5)
+    # Cluster colors of image
+    labels, cluster_colors = gaussian_mixture_cluster(img_rgb, blur_size=blur_size, scale=scale, nr_clusters=nr_clusters)
 
-    for color in cluster_colors.values():
-        # Evaluate Color
-        evaluation, gray, red, green, blue = evaluate_color(color)
+    # PLOT
+    plt.figure(figsize=(10, nr_clusters*2.5))
 
+    # Iterate all cluster colors
+    for index, color in enumerate(cluster_colors.values()):
+        # Evaluate the color by getting color appearance and matching a type to each color appearance
+        map_type, color_appearance = evaluate_color(color)
+
+        # PLOT every color
+
+        # Format every percentage float to string (0.25 -> "25%")
+        for key, value in map_type.items():
+            new_value = "{}%".format(round(value*100))
+            map_type[key] = new_value
+
+        for key, value in color_appearance.items():
+            new_value = "{}%".format(round(value*100))
+            color_appearance[key] = new_value
+
+        # get titles
         title_rgb = "rgb color: {}\n".format(color)
-        title_colors = "gray: {}%, red: {}%, green: {}%, blue: {}%\n".format(round(gray), round(red), round(green),
-                                                                             round(blue))
+        title_colors = str(color_appearance) + "\n"
+        title_labels = str(map_type) + "\n"
+        # make color swatch image
+        color_swatch_img = [[color] * 9]
 
-        background, road, buiding = round(evaluation['background']), round(evaluation['road']), round(evaluation['building'])
+        plt.subplot(nr_clusters, 1, index+1)
+        plt.axis('off')
+        plt.imshow(color_swatch_img)
+        plt.title(title_rgb + title_colors + title_labels, fontsize=18)
 
-        title_labels = "background: {}%, road: {}%, building: {}%".format(background, road, buiding)
+    plt.tight_layout()
 
-        color_swatch_img = [[color] * 6]
-
-        # TODO subplots
-        plt.figure(figsize=(8, 8))
-        plot_images([color_swatch_img], 1, 1, title=title_rgb + title_colors + title_labels)
-
-    (h, w, d) = img_rgb.shape
+    # reshapes one dimensional color array into the two image dimensions
+    h, w, _ = img_rgb.shape
     label_img = labels.reshape(h, w)
+    # makes "empty" image with same dimensions as img_rgb (original)
     img_gm = np.zeros_like(img_rgb)
+    # for every color, change labeled pixels of image to this color
     for lbl, color in cluster_colors.items():
         img_gm[np.where(label_img == lbl)] = color
 
-    plt.figure(figsize=(15, 15))
-    plot_images([img_gm], 1, 1)
+    # PLOT segmented image
+    plt.figure(figsize=(10, 7))
+    plt.subplot(1, 1, 1)
+    plt.suptitle("Segmented Image", fontsize=18)
+    plt.axis('off')
+    plt.imshow(img_gm)
+
+    plt.tight_layout()
+    plt.show()
 
 
-def segment(img_rgb, scale=0.5, blur_size=7, nr_clusters=5):
+# calls gaussian mixture clustering and evaluate color function for each color
+# maps most probable label ('background' 0, 'road' 1, 'building' 2) for each pixel
+# input image, resizing scale, blur scale, and number clusters
+# returns two dim array of image shape with one of following labels: 'background': 0, 'road': 1, 'building': 2 for each pixel
+def segment(img_rgb, scale=0.3, blur_size=7, nr_clusters=5):
+    # calls gaussian mixture clustering
+    cluster_labels, cluster_colors = gaussian_mixture_cluster(img_rgb, blur_size=blur_size, scale=scale, nr_clusters=nr_clusters)
 
-    labels, cluster_colors = gaussian_mixture_cluster(img_rgb, blur_size=blur_size, scale=scale, nr_clusters=nr_clusters)
+    # make zero array like cluster_labels to later fill with map_type labels
+    map_labels = np.zeros_like(cluster_labels)
 
-    max_probs = {'road': 0, 'building': 0, 'background': 0}
-    final_label = {'road': -1, 'building': -1, 'background': -1}
     for lbl, color in cluster_colors.items():
-        # Evaluate Color
-        evaluation, gray, red, green, blue = evaluate_color(color)
+        # evaluate color returns ('background', 'road', 'building') with percentage probability for each label
+        map_type_probability, _ = evaluate_color(color)
+        # get the label with max probability
+        max_map_type = max(map_type_probability, key=map_type_probability.get)
+        type_to_nr = {'background': 0, 'road': 1, 'building': 2}
+        # replace all cluster labels of this color with its map type label
+        map_labels[np.where(cluster_labels == lbl)] = type_to_nr[max_map_type]
 
-        if evaluation['road'] > max_probs['road']:
-            max_probs['road'] = evaluation['road']
-            final_label['road'] = lbl
-        # TODO: other labels
-
-    # TODO: (optional) change label_img to only include (preliminary) labels for road, building and background
-    (h, w, d) = img_rgb.shape
-    label_img = labels.reshape(h, w)
-    return label_img, final_label
+    # reshape one dimensional image into two dimensions of original image
+    h, w, _ = img_rgb.shape
+    map_labels = map_labels.reshape(h, w)
+    return map_labels
 
 
-# Arke: Just a comment, this takes a lot of time. It might be a great method, but finding a faster one is desirable.
-def gaussian_mixture_cluster(img_rgb, blur_size=0, scale=None, nr_clusters=6, b_print=False):
+# CLUSTERS ALL COLORS OF THE IMAGE
+# input rgb image, the blur factor, the scale factor and number of cluster
+# returns labels -> list of all cluster labels for every pixel, cluster_colors -> list of colors for each cluster label
+def gaussian_mixture_cluster(img_rgb, blur_size=0, scale=None, nr_clusters=5):
+    # uses median blur on image to get rid of noise
     if blur_size > 0:
         img_rgb = cv2.medianBlur(img_rgb, blur_size)
 
+    # get one dimensional array of all colors
+    colors_train = colors = img_rgb.reshape((-1, 3))
+
+    # use colors of resized image (to given scale) to train the gaussian mixture
+    # -> faster clustering without loosing color information
     if scale:
-        # Resize image -> faster clustering
-        new_dim = tuple([int(img_rgb.shape[i] * scale) for i in range(2)])
+        height, width = img_rgb.shape[:2]
+        new_dim = (int(width * scale), int(height * scale))
         img_rgb_resized = cv2.resize(img_rgb, new_dim, interpolation=cv2.INTER_AREA)
+        # get one dimensional array of all colors of resized image
         colors_train = img_rgb_resized.reshape((-1, 3))
 
-    else:
-        colors_train = img_rgb.reshape((-1, 3))
-
-    colors_train = np.array(colors_train)
-
-    # how much time does it take
-    start = time.time()
-
-    # Cluster with Gaussian Mixture
+    # cluster colors with Gaussian Mixture
     gm = GaussianMixture(n_components=nr_clusters, random_state=0).fit(colors_train)
 
-    colors = img_rgb.reshape((-1, 3))
-    colors = np.array(colors)
+    # predict the labels on the colors from the original image (not resized)
+    # labels lists the label of the cluster for every pixel color in the image
     labels = gm.predict(colors)
 
-    gm_time = time.time() - start
-    print("Gaussian Mixture took {}s".format(gm_time))
-
-    unique_labels = np.unique(labels)
-
-    if b_print:
-        print("There are {} clusters!".format(len(unique_labels)))
-
+    # calculate the median color value for every cluster
     cluster_colors = {}
-    for k in unique_labels:
+    for k in np.unique(labels):
+        # list where all pixels with the label k are True and rest False
         b_labeled_k = labels == k
-        cluster_colors[k] = np.median(colors[b_labeled_k], axis=0).astype(np.uint8)
-
-        if b_print:
-            print("Label: {} with Color: {}\n".format(k, cluster_colors[k]))
+        # calculates median of colors for where pixels are True
+        # colors as type int32 -> so further calculations can go negative
+        cluster_colors[k] = np.mean(colors[b_labeled_k], axis=0).astype(np.int32)
 
     return labels, cluster_colors
 
 
-# def save_gm_model(img_rgb, no_clusters, file_name):
-#     all_colors = img_rgb.reshape((-1, 3))
-#     # calculate model
-#     # save model
-#     # Check if histograms of model images look similar enough
-#     # proposed models:
-#     #  - urban_dense
-#     #  - urban_sparse
-#     #  - rural_green (for predominantly fields)
-#     #  - rural_blue (for lakes and possibly ocean)
-#     #  - rural_sand (for deserts or barren lands)
-
-# def classify_image(img_rgb):
-#     # compare histogram to prototypical histrogram of each existing model and return best fit model name
-#     model_name = None
-#     return model_name
-
-# returns building/background/road based on color assignment with "percent_gray_red_green_blue"
+# evaluates a type (background, road, building) to each color
+# input a rgb color; return the percentage for each map type and the color appearance
 def evaluate_color(color_rgb):
-    # Arke: Possible code simplification:
-    # is_gray could be a one-liner, removing the need for a function definition
-    # Probably not faster and not necessarily more readable
-    # is_gray = lambda c: (1 - max([abs(c[1] - c[0]), abs(c[2] - c[1]), abs(c[0] - c[2])]) / 255) * 100
-    # TODO: Vectorize (low priority)
+    # get appearance of gray, red, green, blue
+    gray, red, green, blue = get_color_appearance(color_rgb)
 
-    gray = is_gray(color_rgb, min(color_rgb) / 2)  # Why the min_color/2?
-    red, green, blue = is_red_green_blue(color_rgb)
-
-    # gray -> road or building
-    # green and red similar -> background
-
-    if red + green == 0:
-        a = 1
+    if (red + green) == 0:
+        # don't divide by zero -> set it to 1
+        red_green_similarity = 0
     else:
-        a = abs(red - green) / (red + green)
+        # calculate the similarity of red and green -> if very similar (orange tone) -> most likely background
+        red_green_similarity = 1 - (abs(red - green) / (red + green))
 
-    background = (1 - a) * 100
-    not_background = (100 - background)
+    background = red_green_similarity * (1 - gray)
+    # very green values with little red will also be detected as background
+    if green == max([background, red, green, blue]):
+        background = green
+        
+    road = gray
+    building = 1 - background - road
 
-    not_gray = (100 - gray) / 100
+    # set background the similarity of red and green; set road the percentage of gray; set building to the rest
+    map_type = {'background': background, 'road': road, 'building': building}
+    color_appearance = {'gray': gray, 'red': red, 'green': green, 'blue': blue}
 
-    map_type = {'background': background * not_gray, 'road': gray / 2, 'building': not_background * not_gray + gray / 2}
-
-    return map_type, gray, red, green, blue
-
-
-# returns percentage on how gray
-# based on grayish rgb colors have similar values for r, g, b
-# Arke: Suggesting to use 255 as diff_range. this gives similar results to standard deviation
-#       Suggesting to make this an array function (but for small cluster number the performance gain is not significant)
-# Lambda equivalent:
-# lambda c : (1 - max([abs(c[1] - c[0]), abs(c[2] - c[1]), abs(c[0] - c[2])]) / 255) * 100
-def is_gray(color_rgb, diff_range):
-    r, g, b = color_rgb.astype(np.int32)
-    diffs = abs(r - g), abs(r - b), abs(g - b)
-    max_diff = max(diffs)
-    gray_percentage = (1 - (max_diff / diff_range)) * 100
-    return 0 if gray_percentage < 0 else gray_percentage
+    return map_type, color_appearance
 
 
-# Arke:
-# Comparison between min-max approach and standard deviation
-# colorspace = np.array([[r, g, b] for r in range(256) for b in range(256) for g in range(256)])
-# t_start = time.time()
-# grayspace = (1 - np.max(np.hstack((np.abs(colorspace[:, 1] - colorspace[:, 0])[:, np.newaxis],
-#                                    np.abs(colorspace[:, 2] - colorspace[:, 1])[:, np.newaxis],
-#                                    np.abs(colorspace[:, 0] - colorspace[:, 2])[:, np.newaxis])), axis=1) / 255) * 100
-# grayspace[grayspace < 0] = 0
-# t_end = time.time()
-# print("Time to calculate min-max score: " + str(t_end - t_start))
-# max_std = 120.20815280171308
-# t_start = time.time()
-# grayspace_alt = (100 - np.std(colorspace, axis=1) / max_std * 100)
-# t_end = time.time()
-# print("Time to calculate standard deviation: " + str(t_end - t_start))
-# print("Maximum difference: " + str(np.max(grayspace_alt - grayspace)) + ". Minimum difference: " +
-#       str(np.min(grayspace_alt - grayspace)))
-# Conclusion: Standard deviation intuitively makes more sense to me and is more readable, but takes longer to calculate.
-
-
-# returns array r, g, b of 0-100 percentage
+# returns array gray, r, g, b of 0-100 percentage on how the color looks to be more red, green, blue
 # based on the r, g, b with the highest values and their diff to lower values
-def is_red_green_blue(color_rgb, d=0):
-    # Arke: Comment - There is no need to split rgb value and also not for the for loops.
-    # Only really bad for vectorization of the function. For single-value calculation almost no difference.
+def get_color_appearance(color_rgb):
     r, g, b = color_rgb
 
+    # LIGHT GRAY calculation
+
+    # gives a good representation of how gray a color is based on the similarity of rgb values
+    # get the max difference between red, green and blue value
+    max_diff = max(abs(r - g), abs(r - b), abs(g - b))
+    # shows how big the max difference is in comparison to the half of the smallest rgb value
+    diff_comparison = min([r, g, b]) / 2
+    gray = 1 - (max_diff / diff_comparison)
+    if gray < 0:
+        gray = 0
+    # gives a bigger value to light gray than dark gray
+    lightness = np.mean([r, g, b]) / 200
+    if lightness > 1:
+        lightness = 1
+    gray *= lightness
+
+    # RED, GREEN, BLUE calculation
+
     # subtract half max value
-    # -> if lower value less than half of max value -> its too minimal to change color
-    max_value = max([r, g, b])
-    max_diff = int(max_value / 2)
+    # if lower value less than half of max value -> its too minimal to change color
+    # example: rgb (20, 80, 100) -> r=20 < max=100/2=50 -> rgb (0, 30, 50)
+    #          red value irrelevant for color appearance
+    # example: rgb (50, 60, 200) -> r=50 and g=60 < max=200/2=100 -> rgb (0, 0, 100)
+    #          red and green value irrelevant for color appearance
+    max_diff = int(max([r, g, b]) / 2)
     r, g, b = [(x - max_diff if x > max_diff else 0) for x in [r, g, b]]
 
     # subtract min value
-    # -> if the lower two values are similar it just grays the first color and doesn't change it
+    # if the lower two values are similar it just grays the first color and doesn't change it
+    # example: rgb (60, 63, 100) -> subtract min r=60 -> rgb (0, 3, 40)
+    #          red has no and green little influence on the color appearance
+    # example: rgb (40, 90, 200) -> subtract min r=40 -> rgb (0, 50, 160) ->
+    #          red has no but green still has influence on the color appearance
     min_value = min([r, g, b])
     r, g, b = [(x - min_value) for x in [r, g, b]]
 
+    # calculates r, g, b influence in the color
     sum_value = r + g + b
-    r, g, b = [(x / sum_value) * 100 if x > 0 else x for x in [r, g, b]]
+    r, g, b = [(x / sum_value) for x in [r, g, b]]
 
-    # Arke: Shorter, but not significantly faster. But vectorized, so it works for arrays of colors
-    # c = np.array(color_rgb).reshape((-1, 3))  # make sure color is a n x 3 numpy array
-    # c_max = np.max(c, axis=1)
-    # c_min = np.min(c, axis=1)
-    # c_sum = np.sum(c, axis=1)[:, np.newaxis]
-    # c_sub = np.maximum((c_max / 2), c_min)[:, np.newaxis]
-    # tmp = np.maximum(c - c_sub, 0)
-    # return np.divide(tmp2, (c_sum - c_sub)) * 100
-    return [r, g, b]
-
-
-# Arke: Discussing vectorization
-# It would be significantly faster to simultaneously evaluate all colors. However, with only a handful of clusters,
-# the gain is not that big.
-#
-# c = np.tile(np.array([[120, 230, 60]]), (1000, 1))
-# t_start = time.time()
-# tmp1 = np.zeros((1000, 3))
-# for i in range(1000):
-#     tmp1[i,:] = is_red_green_blue(c[i,:])
-# t_end = time.time()
-# print(t_end - t_start)
-# t_start = time.time()
-# c_max = np.max(c, axis=1)
-# c_min = np.min(c, axis=1)
-# c_sum = np.sum(c, axis=1)
-# # c_mid = c_sum - c_max - c_min
-# c_sub = np.maximum((c_max / 2), c_min)[:, np.newaxis]
-# tmp2 = np.maximum(c - np.maximum((c_max / 2), c_min)[:, np.newaxis], 0)
-# tmp2 = np.divide(tmp2, np.sum(tmp2, axis=1)[:, np.newaxis]) * 100
-# t_end = time.time()
-# print(t_end - t_start)
-
-# def test():
-#     img_name = "exp.png"
-#     show_color_evaluation(img_name)
-
-def plot_images(images, r, c, cmap=None, title=None):
-    if title:
-        plt.title(title)
-    for index, image in enumerate(images):
-        plt.subplot(r, c, index + 1)
-        plt.axis('off')
-        plt.imshow(image, cmap=cmap)
-        # plt.tight_layout()
-    plt.tight_layout()
-
-
-def HSV2RGB(img_hsv):
-    return cv2.cvtColor(img_hsv, cv2.COLOR_HSV2RGB)
-
-
-def BGR2RGB(img_bgr):
-    return cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+    return gray, r, g, b
 
 
 if __name__ == "__main__":
