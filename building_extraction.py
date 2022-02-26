40de52474fdd1908369b9e10e86b0b51a1df5c1d
Niels <84444076+nielsvogell@users.noreply.github.com>
2022-02-26 01:52:35 +0100

    split detection function in two. replaced color_eval_img with a tri color image made from the labels

--- building_extraction.py
+++ building_extraction.py
@@ -80,23 +80,7 @@ def morph(img, kernel):
 
 
 
-def building_detection(labeled_img, tresh, min_building_area, nb_buildings=None, output_type='mask', original=None):
-    """ Detects contours of buildings in the image and display an accuracy metric
-    @Author : Tanguy Gerniers (W21)
-    @Args :
-      path (str) : The file location of the image.
-      output_type(str) :  "mask" : no background
-                          "original" : original image as background
-                          "tricolor" : 2 clusters (road network in blue, everything else in green) as background
-      tresh (int) : Determines whether contours should follow contour edges (0) or bound them within smoother
-                    rectangular boxes where possible (120)
-      min_building_area(int) : minimum area a building must be to be considered (recommended : 500-1500, depending on
-                               height from which image is taken)
-      nb_buildings(int) : number of buildings effectively in the image (optional)
-    
-    @Returns :      output (image) : background of choice with contours of detected buildings in red applied on top as a
-                                     mask
-    """
+def process_labeled_img(labeled_img, label_colors=None, output_type='mask', original=None):
     labels = segmentation.get_labels()
     road_mask = labeled_img == labels['road']
     
@@ -127,51 +111,83 @@ def building_detection(labeled_img, tresh, min_building_area, nb_buildings=None,
     background = np.where(road_mask == [0, 0, 0], (0, 255, 0), 0)
     # Make a bicolor mask combining a blue road network & green background
     background_and_roads = (background * 0.5 + roads * 0.5).astype(np.uint8)
-    
-    
-    ### Common code
+
+    # Apply tricolor mask to original image
+    (h, w) = labeled_img.shape
+    if label_colors:
+        col_bg = label_colors['background']
+        col_rd = label_colors['road']
+        col_bd = label_colors['building']
+    else:
+        col_bg = np.array([206, 234, 214], dtype=np.uint8)
+        col_rd = np.array([241, 243, 244], dtype=np.uint8)
+        col_bd = np.array([252, 232, 230], dtype=np.uint8)
+        
+    color_eval_img = np.zeros((h, w, 3), dtype=np.uint8)
+    color_eval_img[np.where(labeled_img == labels['background'])] = col_bg
+    color_eval_img[np.where(road_mask)] = col_rd
+    color_eval_img[np.where(color_eval_img == [0, 0, 0])] = col_bd
+
+    ### Generating output
     if output_type == 'original' and original:
         output = original.copy()
     elif output_type == 'tricolor':
-        # Apply tricolor mask to original image
-        (h, w) = labeled_img.shape
-        output = np.zeros((h, w, 3), dtype=np.uint8)
-        output[np.where(labeled_img == labels['background'])] = np.array([206, 234, 214], dtype=np.uint8)
-        output[np.where(road_mask)] = np.array([241, 243, 244], dtype=np.uint8)
-        output[np.where(output == [0, 0, 0])] = np.array([252, 232, 230], dtype=np.uint8)
-    else: # output_type == 'mask'
+        output = color_eval_img.copy()
+    else:  # output_type == 'mask'
         output = np.zeros_like(labeled_img)
     
-    try:
-        building_mask = color_eval_img == labels['building']
+    return output, color_eval_img
+
+
+def building_detection(labeled_img, tresh, min_building_area, nb_buildings=None, label_colors=None, output_type='mask',
+                       original=None):
+    """ Detects contours of buildings in the image and display an accuracy metric
+    @Author : Tanguy Gerniers (W21)
+    @Args :
+      path (str) : The file location of the image.
+      output_type(str) :  "mask" : no background
+                          "original" : original image as background
+                          "tricolor" : 2 clusters (road network in blue, everything else in green) as background
+      tresh (int) : Determines whether contours should follow contour edges (0) or bound them within smoother
+                    rectangular boxes where possible (120)
+      min_building_area(int) : minimum area a building must be to be considered (recommended : 500-1500, depending on
+                               height from which image is taken)
+      nb_buildings(int) : number of buildings effectively in the image (optional)
+
+    @Returns :      output (image) : background of choice with contours of detected buildings in red applied on top as a
+                                     mask
+    """
     
-    except:
-        imagea = np.where(image == [120, 110, 128], 255, 0)
-        imageb = np.where(image == [42, 48, 66], 255, 0)
-        imagec = np.where(image == [156, 144, 14], 255, 0)
-        building_mask = np.logical_or(imagea, np.logical_or(imageb, imagec)) * 255
-        building_mask = building_mask.astype(np.uint8)
+    labels = segmentation.get_labels()
+    building_mask = labeled_img == labels['building']
     
     # Blur image to make shapes stand out, remove noise & unnecessary details
-    filtered = cv2.bilateralFilter(building_mask, 12, 20, 500)
+    buildings_processed = cv2.bilateralFilter(building_mask, 12, 20, 500)
     
     # Ensure image is binary for morphological operations
-    binary = cv2.threshold(filtered, 101, 255, cv2.THRESH_BINARY)[1]
+    buildings_processed = cv2.threshold(buildings_processed, 101, 255, cv2.THRESH_BINARY)[1]
     
     # Remove secondary structures (large, stuctural noise such as trees, pieces of road that slipped through, etc..)
-    morph1 = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2)), iterations=2)
+    buildings_processed = cv2.morphologyEx(buildings_processed, cv2.MORPH_CLOSE,
+                                           cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2)), iterations=2)
     
     # Reconnect holes within structures to make uniform shapes
-    morph2 = cv2.morphologyEx(morph1, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7)), iterations=2)
+    buildings_processed = cv2.morphologyEx(buildings_processed, cv2.MORPH_OPEN,
+                                           cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7)), iterations=2)
     
     # Increase structure sizes to merge disconnected portions together
-    morph3 = cv2.dilate(morph2, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)), iterations=3)
+    buildings_processed = cv2.dilate(buildings_processed,
+                                     cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)), iterations=3)
     
     # Detect building edges
-    edges = cv2.Canny(morph3, 110, 255)
+    edges = cv2.Canny(buildings_processed, 110, 255)
+    
     
     # Extract contours & hierarchy from edges
     contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Prepare output
+    output, color_eval_img = process_labeled_img(labeled_img, label_colors, output_type, original)
     
     # list storing contours that are not rectangular
     unique_contours = []
@@ -181,25 +197,24 @@ def building_detection(labeled_img, tresh, min_building_area, nb_buildings=None,
     # for each contour (potential building)
     for contour in contours:
         index += 1
-        minx = 10000
-        maxx = 0
-        miny = 10000
-        maxy = 0
-        dx = 0
-        dy = 0
+        min_x = 10000
+        max_x = 0
+        min_y = 10000
+        max_y = 0
+        
         # extract positional information of contour
         for point in contour:
             
-            if point[0][0] < minx:
-                minx = point[0][0]
-            if point[0][0] > maxx:
-                maxx = point[0][0]
-            if point[0][1] < miny:
-                miny = point[0][1]
-            if point[0][1] > maxy:
-                maxy = point[0][1]
-        dx = abs(maxx - minx)
-        dy = abs(maxy - miny)
+            if point[0][0] < min_x:
+                min_x = point[0][0]
+            if point[0][0] > max_x:
+                max_x = point[0][0]
+            if point[0][1] < min_y:
+                min_y = point[0][1]
+            if point[0][1] > max_y:
+                max_y = point[0][1]
+        dx = abs(max_x - min_x)
+        dy = abs(max_y - min_y)
         
         # if contour is not a road (overly long and thin structure) and not within another contour:
         if dx < 3.5 * dy and dy < 3.5 * dx and hierarchy[0][index][3] == -1 and ((dx * dy) > min_building_area):
@@ -211,26 +226,31 @@ def building_detection(labeled_img, tresh, min_building_area, nb_buildings=None,
             x, y, w, h = cv2.boundingRect(contour)
             
             # Compute average color of top/bottom left/right corners of the contour
-            avg_color_cnt = np.array(cv2.mean(color_eval_img[y: y + h, x: x + w])).astype(np.uint8)
-            d0 = int(avg_color_cnt[0]) + int(avg_color_cnt[1]) + int(avg_color_cnt[2])
+            # avg_color_cnt = np.array(cv2.mean(color_eval_img[y:(y + h), x:(x + w)])).astype(np.uint8)
+            # d0 = int(avg_color_cnt[0]) + int(avg_color_cnt[1]) + int(avg_color_cnt[2])
+            # bottom left
             avg_color1 = np.array(cv2.mean(color_eval_img[y: y + int(h / 2), x: x + int(w / 2)])).astype(np.uint8)
             d1 = int(avg_color1[0]) + int(avg_color1[1]) + int(avg_color1[2])
+            # top left
             avg_color2 = np.array(cv2.mean(color_eval_img[y + int(h / 2): y + h, x: x + int(w / 2)])).astype(np.uint8)
             d2 = int(avg_color2[0]) + int(avg_color2[1]) + int(avg_color2[2])
+            # bottom right
             avg_color3 = np.array(cv2.mean(color_eval_img[y: y + int(h / 2), x + int(w / 2): x + w])).astype(np.uint8)
             d3 = int(avg_color3[0]) + int(avg_color3[1]) + int(avg_color3[2])
+            # top right
             avg_color4 = np.array(cv2.mean(color_eval_img[y + int(h / 2): y + h, x + int(w / 2): x + w])).astype(
                 np.uint8)
             d4 = int(avg_color4[0]) + int(avg_color4[1]) + int(avg_color4[2])
             
-            # If corners of contour are sufficiently similar to one another, it is probably a rectangle/trapezoid -> draw smooth & rectangular bounding box
+            # If corners of contour are sufficiently similar to one another, it is probably a rectangle/trapezoid
+            #   -> draw smooth & rectangular bounding box
             if abs(d1 - d2) < tresh and abs(d1 - d3) < tresh and abs(d1 - d4) < tresh and abs(d2 - d3) < tresh and abs(
                     d2 - d4) < tresh and abs(d3 - d4) < tresh:
                 # Draw rectangular building contours, in red, onto chosen background
-                output = cv2.line(output, (minx, miny), (minx, maxy), (255, 0, 0), 3)
-                output = cv2.line(output, (minx, miny), (maxx, miny), (255, 0, 0), 3)
-                output = cv2.line(output, (maxx, maxy), (minx, maxy), (255, 0, 0), 3)
-                output = cv2.line(output, (maxx, maxy), (maxx, miny), (255, 0, 0), 3)
+                output = cv2.line(output, (min_x, min_y), (min_x, max_y), (255, 0, 0), 3)
+                output = cv2.line(output, (min_x, min_y), (max_x, min_y), (255, 0, 0), 3)
+                output = cv2.line(output, (max_x, max_y), (min_x, max_y), (255, 0, 0), 3)
+                output = cv2.line(output, (max_x, max_y), (max_x, min_y), (255, 0, 0), 3)
             
             # If corners of contour are sufficiently different to one another, it probably has a unique shape -> draw contour as it is
             else:
