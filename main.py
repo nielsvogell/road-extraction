2e3726868ebd743c51132edcaccfacd4fd430d3d
Niels <84444076+nielsvogell@users.noreply.github.com>
2022-02-26 17:33:33 +0100

    Update main.py
    extended test and added output file

--- main.py
+++ main.py
@@ -8,20 +8,40 @@ if 'matplotlib' not in sys.modules:
     from matplotlib import use
     use('TkAgg')
 from matplotlib import pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+from importlib import reload
 
 
 def main():
-    img_rgb = cv2.imread('data/munich.png')
+    image_name = 'test01'
+    img_rgb = cv2.cvtColor(cv2.imread(''.join(['data/', image_name, '.png'])), cv2.COLOR_RGB2BGR)
 
-    labeled_img, label_colors = segment(img_rgb)
-    roads = extract_roads(labeled_img)
-    buildings = extract_buildings(labeled_img, output_type='mask')
+    cluster_img, cluster_labels, cluster_colors = segment(img_rgb)
+    roads = extract_roads(cluster_img, cluster_labels, n_largest=7)
+    buildings = extract_buildings(cluster_img, cluster_labels, cluster_colors=cluster_colors, output_type='mask')
     
     img_out = img_rgb.copy()
     img_out[np.where(buildings > 0)] = np.array([250, 120, 120], dtype=np.uint8)
-    img_out[np.where(roads > 0)] = np.array([250, 250, 250], dtype=np.uint8)
+    img_out[np.where(roads > 0)] = np.array([50, 50, 250], dtype=np.uint8)
+
+    grid = ImageGrid(plt.figure(1, figsize=(10, 10)), 111, nrows_ncols=(2, 4), axes_pad=0.1)
+    fig = plt.figure(figsize=(10, 10), tight_layout=True)
+
+    gs = fig.add_gridspec(4, 2)
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax1.imshow(img_rgb)
+    ax2 = fig.add_subplot(gs[0, 1])
+    ax2.imshow(cluster_img)
+    ax3 = fig.add_subplot(gs[1, 0])
+    ax3.imshow(buildings, cmap='gray')
+    ax4 = fig.add_subplot(gs[1, 1])
+    ax4.imshow(roads, cmap='gray')
+    ax5 = fig.add_subplot(gs[2:, :])
+    ax5.imshow(img_out)
+
+    plt.savefig(''.join(['out/', image_name, '_out.png']), dpi=300)
     
-    plt.imshow(img_out)
+    print('Done')
     
     
 # Press the green button in the gutter to run the script.
